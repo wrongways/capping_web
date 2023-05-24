45abb3b9696a1d74a538f444a4039c3aa68a4883
Wrongways <jez@wain.name>
2023-05-24 21:13:29 +0200

    remove unused domain_count rest api

--- src/routes/api/domain_count/+server.ts
+++
@@ -1,5 +0,0 @@
-import { getNumberOfDomains } from '$lib/server/db';
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = () => { return json(getNumberOfDomains()) };
